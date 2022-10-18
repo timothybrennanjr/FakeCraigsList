2c0259ea344b95859d8bdf30e219448265436af4
timothy <timothybrennan8@gmail.com>
2022-10-18 11:48:06 -0400

    Displayed some content from the api on to the page

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { NavBar } from "./NavBar";
 
+
 const Main = () => {
+const [posts, setPosts] = useState([]);
+console.log('posts', posts)
+
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    const response = await fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-FT/posts');
+    const data = await response.json();
+    console.log(data.data.posts)
+    setPosts(data.data.posts);
+  }
+  fetchPosts()
+}, [])
+
   return (
     <div id="main">
       <div id="navbar">
         <div>Contact List</div>
-        <div>Contact List</div>
-        <div>Contact List</div>
       </div>
-      <div id="container">{/* your components here */}</div>
+      <div id="container">
+        <>
+        {
+        posts.map(e => {
+        return(
+          <h2>{e.title}</h2>
+        ) 
+        })
+      }
+        </>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from "react-dom/client"
 import {Main} from './components'
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    createRoutesFromElements,
+} from "react-router-dom"
 
 
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
+
+
 
 root.render(<Main />)
