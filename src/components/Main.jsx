b6813474d746dcf0cfe11b5d6d16a7c0b3ec1ed0
Grant <grant.mc.foster@gmail.com>
2022-10-20 10:48:37 -0500

    setting up browser router

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,41 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { NavBar, Posts, Register, Login, CreatePost } from "./";
-
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  createRoutesFromElements,
+} from "react-router-dom"
 
 const Main = () => {
-  // const [posts, setPosts] = useState([]);
-  // console.log('posts', posts)
 
-  // useEffect(() => {
-  //   const fetchPosts = async () => {
-  //     const response = await fetch('https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-FT/posts');
-  //     const data = await response.json();
-  //     // console.log(data.data.posts)
-  //     setPosts(data.data.posts);
-  //   }
-  //   fetchPosts()
-  // }, [])
+  const router = createBrowserRouter(createRoutesFromElements(
+    <Route path="/" element={<Navbar />}></Route>
+  )
+);
 
   return (
     <div id="main">
-      <div id="navbar">
-        <NavBar />
-        <Register />
-        <Login />
-      </div>
-      <div id="container">
-        <Posts />
-        <CreatePost />
-        {/* <>
-        {
-        posts.map(e => {
-        return(
-          <h2>{e.title}</h2>
-        ) 
-        })
-      }
-        </> */}
-      </div>
+      <RouterProvider router={router}>
+        <div id="navbar">
+          <NavBar />
+          <Register />
+          <Login />
+        </div>
+        <div id="container">
+          <Posts />
+          <CreatePost />
+        </div>
+      </RouterProvider>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from "react-dom/client"
 import {Main} from './components'
-import {
-    createBrowserRouter,
-    RouterProvider,
-    Route,
-    createRoutesFromElements,
-} from "react-router-dom"
+// import {
+//     createBrowserRouter,
+//     RouterProvider,
+//     Route,
+//     createRoutesFromElements,
+// } from "react-router-dom"
 
+// const router = createBrowserRouter(
+//     createRoutesFromElements(
+//         <Route path="/" element={<Main />}></Route>
+//     )
+// );
 
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
@@ -15,3 +20,6 @@ const root = ReactDOM.createRoot(container)
 
 
 root.render(<Main />)
+// root.render(<React.StrictMode>
+//     <RouterProvider router={router}/>
+// </React.StrictMode>)
