d5ec93c98d9bb06a39b992212b653c784dc6d5a9
Grant <grant.mc.foster@gmail.com>
2022-10-20 11:10:58 -0500

    moved router to src/index and testing functionality

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { NavBar, Posts, Register, Login, CreatePost } from "./";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  createRoutesFromElements,
-} from "react-router-dom"
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Route,
+//   createRoutesFromElements,
+// } from "react-router-dom"
 
 const Main = () => {
 
-  const router = createBrowserRouter(createRoutesFromElements(
-    <Route path="/" element={<Navbar />}></Route>
-  )
-);
+//   const router = createBrowserRouter(createRoutesFromElements(
+//     <Route path="/"></Route>
+//   )
+// );
 
   return (
     <div id="main">
-      <RouterProvider router={router}>
+                <NavBar />
+      {/* <RouterProvider router={router}>
         <div id="navbar">
           <NavBar />
           <Register />
@@ -26,7 +27,7 @@ const Main = () => {
           <Posts />
           <CreatePost />
         </div>
-      </RouterProvider>
+      </RouterProvider> */}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
 import React from 'react'
 import ReactDOM from "react-dom/client"
 import {Main} from './components'
-// import {
-//     createBrowserRouter,
-//     RouterProvider,
-//     Route,
-//     createRoutesFromElements,
-// } from "react-router-dom"
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    createRoutesFromElements,
+} from "react-router-dom"
 
-// const router = createBrowserRouter(
-//     createRoutesFromElements(
-//         <Route path="/" element={<Main />}></Route>
-//     )
-// );
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Main />}>
+            <Route path ="NavBar" element={<NavBar />} />
+        </Route>
+
+    )
+);
 
 const container = document.getElementById('app')
 const root = ReactDOM.createRoot(container)
 
 
 
-root.render(<Main />)
-// root.render(<React.StrictMode>
-//     <RouterProvider router={router}/>
-// </React.StrictMode>)
+// root.render(<Main />)
+root.render(<React.StrictMode>
+    <RouterProvider router={router}/>
+</React.StrictMode>)
